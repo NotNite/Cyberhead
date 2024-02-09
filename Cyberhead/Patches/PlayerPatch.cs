cb37746581200b2f98be5c6275df3804f94574ed
NotNite <hi@notnite.com>
2024-02-08 22:58:42 -0500

    Fix random teleports with Slop Crew

--- Cyberhead/Patches/PlayerPatch.cs
+++ Cyberhead/Patches/PlayerPatch.cs
@@ -93,11 +93,16 @@ public class PlayerPatch {
     [HarmonyPatch("SetCharacter")]
     public static void SetCharacter(Player __instance, Characters setChar, int setOutfit = 0) {
         if (!Plugin.CyberheadConfig.General.VrEnabled.Value) return;
+        Plugin.ApplyIk(__instance.characterVisual);
+        if (!__instance.isAI) __instance.characterVisual.head.localScale = Vector3.zero;
     }
 
     [HarmonyPostfix]
     [HarmonyPatch("SetPosAndRotHard")]
     public static void SetPosAndRotHard(Player __instance, Vector3 pos, Quaternion rot) {
+        // We can't determine if a player is a Slop Crew player or not when this is called in player init
+        if (__instance.characterConstructor == null) return;
+
         if (!__instance.isAI && Plugin.XRRig != null) {
             Plugin.XRRig.transform.Find("CameraOffset/XR Camera").GetComponent<XRCamera>().MoveWithPlayer(pos);
         }
