3a3510fbd7ac30c6a9a515aa828395b69630c67f
NotNite <hi@notnite.com>
2024-02-08 23:55:26 -0500

    Hopefully fix wacky cameras with phones

---
+++ Cyberhead/Patches/PlayerPhoneCamerasPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Reptile;
+using UnityEngine;
+
+namespace Cyberhead.Patches;
+
+[HarmonyPatch(typeof(PlayerPhoneCameras))]
+public class PlayerPhoneCamerasPatch {
+    [HarmonyPrefix]
+    [HarmonyPatch("Awake")]
+    [HarmonyBefore("SlopCrew.Plugin.Harmony")]
+    public static bool Awake(PlayerPhoneCameras __instance) {
+        if (!Plugin.CyberheadConfig.General.VrEnabled.Value) return true;
+
+        // Mirror dwelling disabled due to technical difficulties
+        var rearCameraObj = __instance.transform.Find("rearCamera");
+        if (rearCameraObj != null) {
+            var rearCamera = rearCameraObj.GetComponent<Camera>();
+            if (rearCamera != null) rearCamera.enabled = false;
+        }
+
+        var frontCameraObj = __instance.transform.Find("frontCamera");
+        if (frontCameraObj != null) {
+            var frontCamera = frontCameraObj.GetComponent<Camera>();
+            if (frontCamera != null) frontCamera.enabled = false;
+        }
+
+        return false;
+    }
+}
